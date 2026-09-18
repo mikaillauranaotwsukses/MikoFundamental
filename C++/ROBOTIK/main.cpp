61f91f8b0bc75d35267ac4edf01e6cb2a94b4794
mikaillauranaotwsukses <mikailcoding64@gmail.com>
2026-09-18 16:14:56 +0700

    Nambah Batas

--- C++/ROBOTIK/main.cpp
+++ C++/ROBOTIK/main.cpp
@@ -28,33 +28,48 @@ void printMaze(int robotRow, int robotCol) {
 }
 
 int main() {
-    for (int i = 0; i < ROWS; i++) {
-        for (int j = 0; j < COLS; j++) {
-            cout << maze[i][j] << " ";
-        }
-        cout << endl;
-    }
 
     int robotRow = 1;
     int robotCol = 1;
 
-    string commands[] = {"RIGHT", "RIGHT", "DOWN", "DOWN","RIGHT","RIGHT","DOWN","DOWN","UP","UP","UP","UP"};
+    string commands[] = {"RIGHT", "RIGHT", "DOWN", "DOWN"};
+    int totalCommands = sizeof(commands);
 
     cout << "Posisi awal:" << endl;
     printMaze(robotRow, robotCol);
 
-    for (int i = 0; i < 12; i++) {
-        if (commands[i] == "UP") robotRow--;
-        else if (commands[i] == "DOWN") robotRow++;
-        else if (commands[i] == "LEFT") robotCol--;
-        else if (commands[i] == "RIGHT") robotCol++;
+    for (int i = 0; i < totalCommands; i++) {
+    int nextRow = robotRow;
+    int nextCol = robotCol;
 
-        cout << "Setelah perintah " << commands[i] << ":" << endl;
-        printMaze(robotRow, robotCol);
+    if (commands[i] == "UP") nextRow--;
+    else if (commands[i] == "DOWN") nextRow++;
+    else if (commands[i] == "LEFT") nextCol--;
+    else if (commands[i] == "RIGHT") nextCol++;
+
+    if (nextRow < 0 || nextRow >= ROWS || nextCol < 0 || nextCol >= COLS) {
+        cout << "Perintah " << commands[i] << " ditolak: keluar dari batas map!" << endl;
+        continue;
     }
+
+    char nextTile = maze[nextRow][nextCol];
+
+    if (nextTile == '#') {
+        cout << "Perintah " << commands[i] << " ditolak: menabrak tembok!" << endl;
+        continue;
+    }
+
+    if (nextTile == 'X') {
+        cout << "Perintah " << commands[i] << " ditolak: menginjak ranjau!" << endl;
+        continue;
+    }
+
+    robotRow = nextRow;
+    robotCol = nextCol;
+
+    cout << "Setelah perintah " << commands[i] << ":" << endl;
+    printMaze(robotRow, robotCol);
+}
 
     return 0;
 }
-
-
-ini saya coba otak atik kode fullnya. Apakah sudah benar?
